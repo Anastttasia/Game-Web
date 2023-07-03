7111a6df28c1038f3bc1db6ba7ff5c9d90f48134
Anastttasia <nastya.kaneva.99@gmail.com>
2023-07-03 22:29:17 +0300

    Add vite, vue, header

---
+++ vite-project/src/components/Logo.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h2>GAME</h2>
+        <h2>WEB</h2>
+    </div>
+</template>
+
+<style>
+h2 {
+    color: white;
+    font-weight: 100;
+    margin: 0;
+    font-size: 1rem;
+    padding-right: 4rem;
+    line-height: 95%;
+    text-shadow: 
+    0 0 1px #A50ABE,
+    0 0 2px #A50ABE,
+    0 0 5px #A50ABE,
+    0 0 11px #A50ABE,
+    0 0 20px #A50ABE,
+    0 0 30px #A50ABE,
+    0 0 55px #A50ABE,
+    0 0 80px #A50ABE;
+}
+</style>
